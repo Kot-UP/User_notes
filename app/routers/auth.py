7f2c3c26f4d5994ff1bf0b8d649b08ba80defd5f
Aleksandr Kot <triela1991@mail.ru>
2024-08-29 03:01:08 +0700

    Second commit

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -15,11 +15,11 @@ from sqlalchemy import select, insert
 router = APIRouter(prefix='/auth', tags=['auth'])
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/token")
+# oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/token")
+# SECRET_KEY = "a21679097c1ba42e9bd06eea239cdc5bf19b249e87698625cba5e3572f005544"
+# ALGORITHM = 'HS256'
 
-SECRET_KEY = "a21679097c1ba42e9bd06eea239cdc5bf19b249e87698625cba5e3572f005544"
-ALGORITHM = 'HS256'
-
+security = HTTPBasic()
 
 @router.post('/') # создание пользователя
 async def create_user(db: Annotated[AsyncSession, Depends(get_db)], create_user: CreateUser):
@@ -32,39 +32,79 @@ async def create_user(db: Annotated[AsyncSession, Depends(get_db)], create_user:
     }
 
 
-@router.get('/read_current_user')
-async def read_current_user(user: User = Depends(oauth2_scheme)):
+async def get_current_username(db: Annotated[AsyncSession, Depends(get_db)], credentials: HTTPBasicCredentials = Depends(security)) -> dict:
+    user = await db.scalar(select(User).where(User.username == credentials.username))
+
+    if not user or not bcrypt_context.verify(credentials.password, user.hashed_password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
     return user
 
 
-async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get('sub')
-        user_id: int = payload.get('id')
-        expire = payload.get('exp')
-        if username is None or user_id is None:
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail='Could not validate user'
-            )
-        if expire is None:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail="No access token supplied"
-            )
-        if datetime.now() > datetime.fromtimestamp(expire):
-            raise HTTPException(
-                status_code=status.HTTP_403_FORBIDDEN,
-                detail="Token expired!"
-            )
+@router.get('/users/me')
+async def read_current_user(user: dict = Depends(get_current_username)):
+    return {'User': user}
 
-        return {
-            'username': username,
-            'id': user_id,
-        }
-    except JWTError:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail='Could not validate user'
-        )
+
+# @router.post('/token')
+# async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+#     user = await authenticate_user(form_data.username, form_data.password)
+#     if not user:
+#         raise HTTPException(
+#             status_code=status.HTTP_401_UNAUTHORIZED,
+#             detail='Could not validate user'
+#         )
+#     access_token_expires = timedelta(minutes=30)
+#     access_token = create_access_token(
+#         data={"sub": user.username, "id": user.id},
+#         expires_delta=access_token_expires,
+#     )
+#     return {"access_token": access_token, "token_type": "bearer"}
+#
+#
+# async def authenticate_user(username: str, password: str):
+#     async with get_db() as db:
+#         user = await db.execute(select(User).where(User.username == username))
+#         user = user.scalars().first()
+#         if user and bcrypt_context.verify(password, user.hashed_password):
+#             return user
+#     return None
+#
+#
+# def create_access_token(data: dict, expires_delta: timedelta):
+#     expire = datetime.utcnow() + expires_delta
+#     data.update({"exp": expire})
+#     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+#     return encoded_jwt
+#
+#
+# async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+#     try:
+#         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+#         username: str = payload.get('sub')
+#         user_id: int = payload.get('id')
+#         expire = payload.get('exp')
+#         if username is None or user_id is None:
+#             raise HTTPException(
+#                 status_code=status.HTTP_401_UNAUTHORIZED,
+#                 detail='Could not validate user'
+#             )
+#         if expire is None:
+#             raise HTTPException(
+#                 status_code=status.HTTP_400_BAD_REQUEST,
+#                 detail="No access token supplied"
+#             )
+#         if datetime.now() > datetime.fromtimestamp(expire):
+#             raise HTTPException(
+#                 status_code=status.HTTP_403_FORBIDDEN,
+#                 detail="Token expired!"
+#             )
+#
+#         return {
+#             'username': username,
+#             'id': user_id,
+#         }
+#     except JWTError:
+#         raise HTTPException(
+#             status_code=status.HTTP_401_UNAUTHORIZED,
+#             detail='Could not validate user'
+#         )
--- app/routers/note.py
+++ app/routers/note.py
@@ -1,7 +1,7 @@
 from sqlalchemy import insert, select, update
 from fastapi import APIRouter, Depends, status, HTTPException
 from typing import Annotated
-from app.routers.auth import get_current_user
+from app.routers.auth import get_current_username
 from app.backend.db_depends import get_db
 from app.models import *
 from app.schemas.note import CreateNote
@@ -12,19 +12,19 @@ import pyaspeller
 
 
 router = APIRouter(prefix="/notes", tags=["notes"])
-
 speller = pyaspeller.YandexSpeller(lang='ru')
+
 
 @router.get('/all_notes')
 async def all_notes(db: Annotated[AsyncSession, Depends(get_db)],
-                    get_user: Annotated[dict, Depends(get_current_user)]):
+                    get_user: Annotated[dict, Depends(get_current_username)]):
 
     if get_user.get('user'):
         notes = await db.scalars(select(Note).where(Note.user_note))
         if notes is None:
             raise HTTPException(
                 status_code=status.HTTP_404_NOT_FOUND,
-                detail="There are no notes"
+                detail='There are no notes'
             )
         return notes.all()
     else:
@@ -36,16 +36,18 @@ async def all_notes(db: Annotated[AsyncSession, Depends(get_db)],
 
 @router.post('/create')
 async def create_note(db: Annotated[AsyncSession, Depends(get_db)], create_note: CreateNote,
-                         get_user: Annotated[dict, Depends(get_current_user)]):
-    if get_user.get('user'):
+                         get_user: Annotated[dict, Depends(get_current_username)]) -> dict:
+
+    if get_user.get('username'):
         try:
             result = speller.spell(create_note.user_note)
             if result:
-                return {'error' : 'Орфографическая ошибка '}
+                return {'error' : 'Орфографическая ошибка'}
         except Exception as e:
             return {'error' : e}
 
-        await db.execute(insert(Note).values(user_note=create_note.user_note))
+        await db.execute(insert(Note).values(user_note=create_note.note))
+        #await db.execute(insert(Note).values(user_id=get_user['user'].id, note=create_note.user_note))
         await db.commit()
         return {
             'status_code': status.HTTP_201_CREATED,
